5daa528b8f95684fbb85c6fd41eb15354457941d
Craig Watkinson <craig.watkinson@cambridgeappsolutions.com>
2013-07-04 22:21:08 +0100

    Changed background colour to yellow

--- DCTAuth Demo/AppDelegate.m
+++ DCTAuth Demo/AppDelegate.m
@@ -17,7 +17,7 @@
 	NSLog(@"%@", [[DCTAuthAccountStore defaultAccountStore] accounts]);
 	
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor yellowColor];
 	self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[AuthTestViewController new]];
     [self.window makeKeyAndVisible];
     return YES;
